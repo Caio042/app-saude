b726d7a7eed7878d41b9f782201edf1cdc766a30
Caio042 <caiohenriquegomeslima@gmail.com>
2021-09-26 23:06:25 -0300

    Criação de redirecionador de telas na principal

--- MainActivity.cs
+++ MainActivity.cs
@@ -4,14 +4,16 @@ using Android.Runtime;
 using Android.Widget;
 using AndroidX.AppCompat.App;
 using System;
+using System.Collections;
 
 namespace app_saude
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-        Button btnGorduraCorporal, btnAgua;
-        TextView lblCaloriasPergunta, lblCaloriasResposta;
+        ListView listView1;
+        ArrayList telas;
+        ArrayAdapter adaptador;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -20,10 +22,42 @@ namespace app_saude
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
 
-            
-            
+            listView1 = FindViewById<ListView>(Resource.Id.listView1);
+            telas = new ArrayList();
+            telas.Add("Calculadora de IMC");
+            telas.Add("Verificador de gordura corporal");
+            telas.Add("Verificador de hidratação diaria");
+            telas.Add("Emagrecer, manter ou engordar");
+            adaptador = new ArrayAdapter(this,
+                Android.Resource.Layout.SimpleListItem1, telas);
+
+            listView1.Adapter = adaptador;
+
+            listView1.ItemClick += ListView1_ItemClick;
 
         }
+
+        private void ListView1_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+            int item = e.Position;
+
+            switch (item)
+            {
+                case 0:
+                    StartActivity(typeof(ImcActivity));
+                    break;
+                case 1:
+                    StartActivity(typeof(GorduraActivity));
+                    break;
+                case 2:
+                    StartActivity(typeof(AguaActivity));
+                    break;
+                case 3:
+                    StartActivity(typeof(CaloriaActivity));
+                    break;
+            }
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
